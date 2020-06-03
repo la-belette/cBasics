54419502c0e8024a7501b443c91507e8bd66450c
la-belette <helene.delabroise@gmail.com>
2020-06-02 21:47:53 -0700

    added bit operation rol, ror, my_xor_crypt, my_rol_crypt, my_ror_crypt and corresponding tests

---
+++ bit_operations_tests.c
@@ -0,0 +1,207 @@
+#include "bit_operations_tests.h"
+#include "bit_operation_helper.h"
+#include "common.h"
+#include "memory_manager.h"
+
+int test_rol()
+{
+    int err = 0;
+
+    uint8_t target = 0b1;
+    uint8_t shift = 6;
+    uint8_t expected_result = 0b1000000;
+    uint8_t result = rol(target, shift);
+    if (result != expected_result)
+    {
+        printf("fail in %s: %d bits shift of %d result is %d instead of expected %d.\n",
+                __func__,
+                shift,
+                target,
+                result,
+                expected_result);
+        err = -1;
+    }
+
+    target = 0b1011010;
+    shift = 1;
+    expected_result = 0b10110100;
+    result = rol(target, shift);
+    if (result != expected_result)
+    {
+        printf("fail in %s: %d bits shift of %d result is %d instead of expected %d.\n",
+               __func__,
+               shift,
+               target,
+               result,
+               expected_result);
+        err = -1;
+    }
+
+    target = 0b11111111;
+    shift = 8;
+    result = rol(target, shift);
+    if (result != target)
+    {
+        printf("fail in %s: %d bits shift of %d result is %d instead of expected %d.\n",
+               __func__,
+               shift,
+               target,
+               result,
+               expected_result);
+        err = -1;
+    }
+    return 0;
+}
+
+int test_ror()
+{
+    int err = 0;
+
+    uint8_t target = 0b1000000;
+    uint8_t shift = 6;
+    uint8_t expected_result = 0b1;
+    uint8_t result = ror(target, shift);
+    if (result != expected_result)
+    {
+        printf("fail in %s: %d bits shift of %d result is %d instead of expected %d.\n",
+               __func__,
+               shift,
+               target,
+               result,
+               expected_result);
+        err = -1;
+    }
+
+    target = 0b10110100;
+    shift = 1;
+    expected_result = 0b1011010;
+    result = ror(target, shift);
+    if (result != expected_result)
+    {
+        printf("fail in %s: %d bits shift of %d result is %d instead of expected %d.\n",
+               __func__,
+               shift,
+               target,
+               result,
+               expected_result);
+        err = -1;
+    }
+
+    target = 0b11111111;
+    shift = 8;
+    result = ror(target, shift);
+    if (result != target)
+    {
+        printf("fail in %s: %d bits shift of %d result is %d instead of expected %d.\n",
+               __func__,
+               shift,
+               target,
+               result,
+               expected_result);
+        err = -1;
+    }
+    return 0;
+}
+
+void char_array_print(unsigned char *array, size_t size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", array[i]);
+    }
+    printf("\n");
+}
+
+int test_xor_crypt()
+{
+    int err = 0;
+
+    char *target = malloc(7* sizeof(char));
+    target[0] = 'E';
+    target[1] = 'x';
+    target[2] = 'a';
+    target[3] = 'm';
+    target[4] = 'p';
+    target[5] = 'l';
+    target[6] = 'e';
+    char *key = "Key";
+    char expected_output[] = {14, 29, 24, 38, 21, 21, 46};
+    my_xor_crypt(target, 7, key, 3);
+    if (0 != memcmp(target, expected_output, 7))
+    {
+        printf("fail in %s: xor crypt result is\n", __func__);
+        char_array_print(target, 7);
+        printf("instead of expected\n");
+        char_array_print (expected_output, 7);
+        err = -1;
+    }
+
+    return err;
+}
+
+int test_rol_crypt()
+{
+    int err = 0;
+
+    unsigned char *target = malloc(7* sizeof(char));
+    target[0] = 'E';
+    target[1] = 'x';
+    target[2] = 'a';
+    target[3] = 'm';
+    target[4] = 'p';
+    target[5] = 'l';
+    target[6] = 'e';
+    unsigned char *key = "Key";
+    unsigned char expected_output[] = {42, 15, 194, 107, 14, 216, 43};
+    my_rol_crypt(target, 7, key, 3);
+    if (0 != memcmp(target, expected_output, 7))
+    {
+        printf("fail in %s: rol crypt result is\n", __func__);
+        char_array_print(target, 7);
+        printf("instead of expected\n");
+        char_array_print (expected_output, 7);
+        err = -1;
+    }
+
+    return err;
+}
+
+int test_ror_crypt()
+{
+    int err = 0;
+
+    unsigned char *target = malloc(7* sizeof(char));
+    target[0] = 'E';
+    target[1] = 'x';
+    target[2] = 'a';
+    target[3] = 'm';
+    target[4] = 'p';
+    target[5] = 'l';
+    target[6] = 'e';
+    unsigned char *key = "Key";
+    unsigned char expected_output[] = {168, 195, 176, 173, 131, 54, 172};
+    my_ror_crypt(target, 7, key, 3);
+    if (0 != memcmp(target, expected_output, 7))
+    {
+        printf("fail in %s: ror crypt result is\n", __func__);
+        char_array_print(target, 7);
+        printf("instead of expected\n");
+        char_array_print (expected_output, 7);
+        err = -1;
+    }
+
+    return err;
+}
+
+int test_bit_operations()
+{
+    int err = 0;
+
+    err += test_rol();
+    err += test_ror();
+    err += test_xor_crypt();
+    err += test_rol_crypt();
+    err += test_ror_crypt();
+
+    return err;
+}
---
+++ bit_operations_tests.h
@@ -0,0 +1,10 @@
+#ifndef CBASICS_BIT_OPERATIONS_TESTS_H
+#define CBASICS_BIT_OPERATIONS_TESTS_H
+
+/*
+ * tests functions in bit_operations_helper files.
+ * @return 0 on success, -1 on failure.
+ */
+int test_bit_operations();
+
+#endif //CBASICS_BIT_OPERATIONS_TESTS_H
